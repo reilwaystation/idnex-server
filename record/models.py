36447a365ca8f5e60919575b8cb7e6e32691b4be
reilwaystation <reilwaystation@gmail.com>
2021-01-30 21:18:53 +0800

    make null to blanks

--- record/models.py
+++ record/models.py
@@ -5,19 +5,18 @@ from django.utils import timezone
 
 
 class Person(models.Model):
-    email = models.EmailField(max_length=255, blank=True, null=True)
+    email = models.EmailField(max_length=255, blank=True)
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
-    middlename = models.CharField(max_length=255, blank=True, null=True)
-    extension = models.CharField(max_length=255, blank=True, null=True)
+    middlename = models.CharField(max_length=255, blank=True)
+    extension = models.CharField(max_length=255, blank=True)
     birthdate = models.DateTimeField(blank=True, null=True)
-    birthplace = models.CharField(max_length=255, blank=True, null=True)
-    status = models.CharField(max_length=255, blank=True, null=True)
-    gender = models.CharField(max_length=255, blank=True, null=True)
-    phone = models.IntegerField(default=0, blank=True, null=True)
-    nationality = models.CharField(max_length=255, blank=True, null=True)
-    spouse = models.ForeignKey(
-        'self', blank=True, on_delete=models.SET_NULL, null=True)
+    birthplace = models.CharField(max_length=255, blank=True)
+    status = models.CharField(max_length=255, blank=True)
+    gender = models.CharField(max_length=255, blank=True)
+    phone = models.IntegerField(default=0, blank=True)
+    nationality = models.CharField(max_length=255, blank=True)
+    spouse = models.ForeignKey('self', on_delete=models.SET_NULL, null=True)
     thumbnail = ProcessedImageField(
         upload_to='profiles',
         processors=[ResizeToFill(256, 256)],
@@ -29,14 +28,14 @@ class Person(models.Model):
 
 
 class Address(models.Model):
-    number = models.IntegerField(default=0, null=True, blank=True)
-    street = models.CharField(max_length=255, null=True, blank=True)
+    number = models.IntegerField(default=0, blank=True)
+    street = models.CharField(max_length=255, blank=True)
     barangay = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     province = models.CharField(max_length=255)
-    classification = models.CharField(max_length=255,  null=True, blank=True)
-    longitude = models.FloatField(max_length=255,  null=True, blank=True)
-    latitude = models.FloatField(max_length=255,  null=True, blank=True)
+    classification = models.CharField(max_length=255,   blank=True)
+    longitude = models.FloatField(max_length=255,   blank=True)
+    latitude = models.FloatField(max_length=255,   blank=True)
 
     def __str__(self):
         return f"{self.barangay}, {self.city}, {self.province}"
@@ -45,10 +44,10 @@ class Address(models.Model):
 class Ownership(models.Model):
     owner = models.ForeignKey(Person, on_delete=models.CASCADE)
     address = models.ForeignKey(Address, on_delete=models.CASCADE)
-    classification = models.CharField(max_length=255, unique=False)
-    price = models.FloatField(max_length=255, unique=False)
-    previous = models.FloatField(max_length=255, unique=False)
-    acquired = models.DateTimeField(default=timezone.now)
+    classification = models.CharField(max_length=255, blank=True)
+    price = models.FloatField(max_length=255, blank=True)
+    previous = models.FloatField(max_length=255, blank=True)
+    acquired = models.DateTimeField(blank=True)
 
     def __str__(self):
         return f"{self.owner.firstname} {self.owner.lastname}"
