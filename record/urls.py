5d217a556fa6f7ecdcbb62ebfaac45e0b3ff4866
reilwaystation <reilwaystation@gmail.com>
2021-01-30 17:05:18 +0800

    add record app

--- record/urls.py
+++ record/urls.py
@@ -3,9 +3,9 @@ from .views import PersonList, PersonDetail, AddressList, AddressDetail, Ownersh
 
 urlpatterns = [
     path('address', AddressList.as_view()),
-    path('ownerhip', OwnershipList.as_view()),
+    path('ownership', OwnershipList.as_view()),
     path('person', PersonList.as_view()),
     path('person/<person>', PersonDetail.as_view()),
     path('address/<address>', AddressDetail.as_view()),
-    path('ownerhip/<ownerhip>', OwnershipDetail.as_view()),
+    path('ownership/<ownership>', OwnershipDetail.as_view()),
 ]
